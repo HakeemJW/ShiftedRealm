05a2bb4d1920081d515f908f0343602c870a6085
HakeemJW <wilsonhakeem@gmail.com>
2022-11-05 22:39:08 -0400

    removed more namespace std

--- resources/source/character.cpp
+++ resources/source/character.cpp
@@ -11,15 +11,14 @@
 	Currently needs to be further simplified.
 */
 
-using namespace std;
 int gold = 100;
-string location[4] = { "Dornin", "Fanrir", "Jagel", "Winona" };
-string current_location = location[0];
-string weapons[4] = { "Sword", "Axe", "Daggar", "Mace" };
-string cost[4] = { "10", "15", "20", "25" };
-string armor[4] = {"Chain Mail", "Plate Mail", "Full Plate", "Robes"};
-string equipment[4] = { "Rope", "Torch", "Pole", "Hooks" };
-string potions[4] = { "Healing", "Fire Breathing", "Stealth", "Strength" };
+std::string location[4] = { "Dornin", "Fanrir", "Jagel", "Winona" };
+std::string current_location = location[0];
+std::string weapons[4] = { "Sword", "Axe", "Daggar", "Mace" };
+std::string cost[4] = { "10", "15", "20", "25" };
+std::string armor[4] = {"Chain Mail", "Plate Mail", "Full Plate", "Robes"};
+std::string equipment[4] = { "Rope", "Torch", "Pole", "Hooks" };
+std::string potions[4] = { "Healing", "Fire Breathing", "Stealth", "Strength" };
 combat* com = nullptr;
 
 //Player class selection
@@ -32,7 +31,7 @@ void character::pickclass()
 //Player names character
 void character::namechar()
 {
-	string name;
+	std::string name;
 	Print("What is your name?");
 	std::getline(std::cin,name);
 	Print("\n");
@@ -60,7 +59,7 @@ void character::print_equipment_owned()
 	Print("Slot \tWeapons Owned \t Armor Owned \t Equipment\n");
 	for (int i = 0; i < 5; i++)
 	{
-		cout << "(" << i << ")" << "\t" << weapons_owned[i] << "\t" << armor_owned[i] << "\t" << equipment_owned[i] << endl;
+		std::cout << "(" << i << ")" << "\t" << weapons_owned[i] << "\t" << armor_owned[i] << "\t" << equipment_owned[i] << std::endl;
 	}
 }
 
@@ -80,22 +79,22 @@ void character::equip()
 			case 1:
 				for (int i = 0; i < 5; i++)
 				{
-					cout << "(" << i << ")" << "\t" << weapons_owned[i] << endl;
+					std::cout << "(" << i << ")" << "\t" << weapons_owned[i] << std::endl;
 				}
-				cout << "Which slot?\n";
-				cin >> y;
+				Print("Which slot?");
+				std::cin >> y;
 				weapon_equipped = weapons_owned[y];
-				cout << weapon_equipped << " is now equipped.\n";
+				Print2(weapon_equipped," is now equipped.");
 				break;
 			case 2:
 				for (int i = 0; i < 5; i++)
 				{
-					cout << "(" << i << ")" << "\t" << armor_owned[i] << endl;
+					std::cout << "(" << i << ")" << "\t" << armor_owned[i] << std::endl;
 				}
-				cout << "Which slot?\n";
-				cin >> y;
+				Print("Which slot?");
+				std::cin >> y;
 				armor_equipped = armor_owned[y];
-				cout << armor_equipped << " is now equipped.\n";
+				Print2(armor_equipped," is now equipped.");
 				break;
 		}
 				
@@ -119,53 +118,53 @@ void character::action(character* c)
 		switch (_getch())
 		{
 		case KEY_M: //Opens Menu
-			cout << "What would you like to do\n\n";
-			cout << "Shop(1)  Travel(2)  Adventure(3)  Rest(4)  Check Stats(5)  Check Inventory(6)" << endl;
-			cin >> x;
+			Print("What would you like to do\n");
+			Print("Shop(1)  Travel(2)  Adventure(3)  Rest(4)  Check Stats(5)  Check Inventory(6)");
+			std::cin >> x;
 			switch (x)
 			{
 			case 1: //Shopping Options
-				cout << "You have " << gold << " gold\n\n";
-				cout << "What do you desire?\n\n";
-				cout << "Weapons (1) \t Armor(2) \t Equipment(3) \t Potions(4)\n\n";
+				std::cout << "You have " << gold << " gold\n\n";
+				Print("What do you desire?\n");
+				Print("Weapons (1) \t Armor(2) \t Equipment(3) \t Potions(4)\n");
 				int f;
-				cin >> f;
+				std::cin >> f;
 				switch (f) // Prints list of all available items for purchase
 				{
 				case 1:
 					int p;
 					for (int i = 0; i < 4; i++)
 					{
-						cout << "(" << i << ")" << weapons[i];
-						cout << " (" << cost[i] << ")\n";
+						std::cout << "(" << i << ")" << weapons[i];
+						std::cout << " (" << cost[i] << ")\n";
 					}
 					//Making the item purchase
-					cout << "Which weapon would you like?\n";
-					cin >> p;
-					cout << "Which weapon slot would you like to overwrite?" << endl;
+					Print("Which weapon would you like?");
+					std::cin >> p;
+					Print("Which weapon slot would you like to overwrite?");
 					int n;
-					cin >> n;
+					std::cin >> n;
 					weapons_owned[n] = weapons[p];
 					break;
 				case 2:
 					for (int i = 0; i < 4; i++)
 					{
-						cout << "(" << i << ")" << armor[i];
-						cout << " (" << cost[i] << ")\n";
+						std::cout << "(" << i << ")" << armor[i];
+						std::cout << " (" << cost[i] << ")\n";
 					}
 					break;
 				case 3:
 					for (int i = 0; i < 4; i++)
 					{
-						cout << "(" << i << ")" << equipment[i];
-						cout << " (" << cost[i] << ")\n";
+						std::cout << "(" << i << ")" << equipment[i];
+						std::cout << " (" << cost[i] << ")\n";
 					}
 					break;
 				case 4:
 					for (int i = 0; i < 4; i++)
 					{
-						cout << potions[i];
-						cout << " (" << cost[i] << ")\n";
+						Print(potions[i]);
+						std::cout << " (" << cost[i] << ")\n";
 					}
 					break;
 				default: break;
@@ -173,44 +172,44 @@ void character::action(character* c)
 				break;
 			case 2: //Alow player to move between locations
 				int location_choice;
-				cout << "You are currently located in " << current_location << ".\n\n";
-				cout << "Where would you like to go?\n\n";
+				Print2("You are currently located in ",current_location);
+				Print("Where would you like to go?\n");
 				for (int i = 0; i < 4; i++)
 				{
-					cout << location[i];
-					cout << " (" << j << ")\n\n";
+					Print(location[i]);
+					std::cout << " (" << j << ")\n\n";
 					j++;
 				}
 				j = 0;
-				cin >> location_choice;
+				std::cin >> location_choice;
 				current_location = location[location_choice];
-				cout << "Your new location is " << current_location << "!\n\n";
+				std::cout << "Your new location is " << current_location << "!\n\n";
 				break;
 			case 3://Story Progression
-				cout << "What type of adventure do you seek?\n\n";
-				cout << "Bounty(1) \t Dungeon Crawl(2)\n\n";
+				Print("What type of adventure do you seek?\n");
+				Print("Bounty(1) \t Dungeon Crawl(2)\n");
 				int fp; 
-				cin >> fp;
+				std::cin >> fp;
 				switch (fp)
 				{
 				case 1:
-					cout << "Here are the available bounties in this town.\n\n";
+					Print("Here are the available bounties in this town.\n");
 					com->fight(c);
 					break;
 				case 2:
-					cout << "Here is a list of nearby dungeons.\n\n";
+					Print("Here is a list of nearby dungeons.\n");
 					break;
 				default: break;
 				}
 				break;
 			case 4:
-				cout << "You are fully rested\n\n";
+				Print("You are fully rested\n");
 				break;
 			case 5://Allows player to check stats manually
-				cout << "Health: " << c->health << endl;
-				cout << "Attack: " << c->attack << endl;
-				cout << "Defense: " << c->defense << endl;
-				cout << "Experience: " << c->exp << endl;
+				Print2("Health: ",c->health);
+				Print2("Attack: ",c->attack);
+				Print2("Defense: ",c->defense);
+				Print2("Experience: ",c->exp);
 				break;
 			case 6://Allows Players to view and equip items
 				print_equipped();
